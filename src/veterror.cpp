9f7e1d680d7e2b542176542ab755a030f4b3437b
TooSchoolForCool <zeyuz@outlook.com>
2017-03-15 00:01:41 +0800

    Bug fixed

--- src/veterror.cpp
+++ src/veterror.cpp
@@ -1,4 +1,27 @@
-#include "error.h"
+/**
+* [
+*  Copyright (c) 2016 by Vehicle-Eye Technology
+*  ALL RIGHTS RESERVED.
+*
+*  The software and information contained herein are proprietary to,  and comprise
+*  valuable trade secrets of, Vehicle-Eye Technology.  Disclosure of the software
+*  and information will materially harm the business of Vehicle-Eye Technology.
+*  This software is furnished pursuant to a written development agreement and may
+*  be used, copied, transmitted, and stored only in accordance with the terms of
+*  such agreement and with the inclusion of the above copyright notice.  This
+*  software and information or any other copies thereof may not be provided or
+*  otherwise made available to any other party.
+* ]
+*/
+
+/*!
+* \file veterror.cpp
+* \author [Zeyu Zhang]
+* \version [0.1]
+* \date 2016-02-22
+*/
+
+#include "veterror.h"
 
 using namespace std;
 
